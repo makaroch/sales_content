cd5a6466374c45ad2ee1cf8a7dbf4e4604a911c5
Alex <da.ek.ned>
2025-05-26 22:42:34 +0300

    fix final

--- adminka/app/signals.py
+++ adminka/app/signals.py
@@ -1,4 +1,5 @@
 import io
+import threading
 
 import requests
 
@@ -11,20 +12,34 @@ from adminka.settings import BOT_TOKEN
 @receiver(post_save, sender=InputText)
 def mymodel_created_handler(sender, instance, created, **kwargs):
     if created:
-        tg_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendDocument'
-        all_spam_doc = list(BinaryDocument.objects.filter(status_file="RASS").all())
-        all_clients = list(Client.objects.filter().all())
-        text = instance.text_input
-        for client in all_clients:
+        threading.Thread(target=spam, args=(instance.text_input,)).start()
+
+
+def spam(text):
+    tg_url_doc = f'https://api.telegram.org/bot{BOT_TOKEN}/sendDocument'
+    tg_url_message = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    all_spam_doc = list(BinaryDocument.objects.filter(status_file="RASS").all())
+    all_clients = list(Client.objects.filter().all())
+    for client in all_clients:
+        if all_spam_doc:
             for doc in all_spam_doc:
                 file_name = f"{doc.name}.{doc.expansion}"
                 file_like = io.BytesIO(doc.file_data)
                 file_like.name = file_name
                 try:
-                    requests.post(
-                        url=tg_url,
+                    result = requests.post(
+                        url=tg_url_doc,
                         data={'chat_id': client.id, "caption": text},
                         files={'document': (file_name, file_like)}
                     )
+                    print(result)
                 except Exception as e:
                     print(e)
+        else:
+            payload = {
+                'chat_id': client.id,
+                'text': text,
+                'parse_mode': 'HTML'
+            }
+            response = requests.post(tg_url_message, data=payload)
+            print(response)
